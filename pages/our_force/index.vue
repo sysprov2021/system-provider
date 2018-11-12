8b88eb91b8a34bfbadb167a748ae684820c29158
Roman Kuba <roman.kuba@gmail.com>
2018-11-12 14:50:26 +0100

    Added content for some new pages

---
+++ pages/our_force/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-html="html" />
+  </div>
+</template>
+
+<script>
+import fm from './content.md'
+
+  export default {
+    data() {
+      return {
+        html: fm.html
+      }
+    }
+  }
+</script>
---
+++ pages/references/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>References</h1>
+    <ul>
+      <li
+        v-for="(artist, key) in artists"
+        :key="key"
+      >
+        {{ artist }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import artists from '~/static/graph.json'
+
+export default {
+  data() {
+    return {
+      artists
+    }
+  }
+}
+</script>
+
