44cfb99bef38b0ff4169c753bff33286881b5b02
Roman Kuba <roman.kuba@gmail.com>
2018-12-14 21:08:59 +0100

    Started to add support for image resizing

--- bin/graph.js
+++ bin/graph.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const yaml = require('js-yaml')
+const sharp = require('sharp')
 
 const DEFAULT_STRUCT = {
   date: '',
@@ -9,13 +10,41 @@ const DEFAULT_STRUCT = {
   spc: [],
 }
 
+const IMAGE_VALUES = {
+  title: 1200,
+  thumbnail: 200
+}
+
 const staticPath = path.join(__dirname, '..', '/static')
 const dataPath = path.join(__dirname, '..', '/data')
 const artistsPath = path.join(dataPath, '/artists')
 const graph = {}
 
+function processImages(currentPath, files) {
+  files.forEach(file => {
+    if(file === 'meta.yml') return
+    const fileExtension = path.extname(file)
+    const fileName = path.basename(file, fileExtension)
+    const fileIsImage = ['.jpg', '.jpeg', '.png'].includes(fileExtension)
+    if (!fileIsImage) return
+    const inputFile = path.join(currentPath, '/', file)
+    const outputFile = path.join(currentPath, `/_${fileName}${fileExtension}`)
+
+    sharp(inputFile)
+      .resize(IMAGE_VALUES[fileName] || IMAGE_VALUES.thumbnail)
+      .toFile(outputFile);
+  })
+}
+
+// TODO: Process Images for title image only and gallery images seperately
+
 function processFolder(folder) {
-  const metaFilePath = path.join(artistsPath, '/', folder, '/meta.yml')
+  const currentPath = path.join(artistsPath, '/', folder)
+  const metaFilePath = path.join(currentPath, '/meta.yml')
+  fs.readdir(currentPath, (err, files) => {
+    processImages(currentPath, files)
+  })
+
 
   try {
     const metaFileStream = fs.readFileSync(metaFilePath, 'utf8')
