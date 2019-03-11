932416f1ce0dd282e4508b07310b2820ed058631
Roman Kuba <roman.kuba@gmail.com>
2019-03-11 21:03:31 +0100

    Added reference filter support

--- pages/references/index.vue
+++ pages/references/index.vue
@@ -1,6 +1,24 @@
 <template>
   <Content title="References">
     <div class="references">
+      <div class="filters">
+        <h4>Filter the refernces:</h4>
+        <input
+          v-model="filterValue"
+          placeholder="Name..."
+          type="search"
+        >
+        <button
+          :class="{ active: filterType === 'prod' }"
+          @click="filterBy('prod')">Production</button>
+        <button
+          :class="{ active: filterType === 'tour' }"
+          @click="filterBy('tour')">Tour</button>
+        <button
+          :class="{ active: filterType === 'local' }"
+          @click="filterBy('local')">Local</button>
+        <button @click="resetFilter">Show all </button>
+      </div>
       <Reference
         v-for="artist in artists"
         :key="artist[0]"
@@ -23,7 +41,41 @@ export default {
 
   data() {
     return {
-      artists: allArtists
+      filterValue: '',
+      filterType: undefined
+    }
+  },
+
+  computed: {
+    filter() {
+      return this.filterValue.trim()
+    },
+
+    artists() {
+      let segment = allArtists
+      if (this.filterType) {
+        segment = segment.filter(artist => {
+          return artist[1].type === this.filterType
+        })
+      }
+      if (this.filter.length) {
+        segment = segment.filter(artist => {
+          return artist[1].name.toLowerCase().indexOf(this.filter) >= 0
+        })
+      }
+
+      return segment
+    }
+  },
+
+  methods: {
+    resetFilter() {
+      this.filterType = undefined
+    },
+
+    filterBy(type) {
+      this.filterType = type
+      this.filterValue = ''
     }
   },
 }
@@ -31,7 +83,29 @@ export default {
 
 
 <style lang="sass" scoped>
-.references
+.filters
+  padding: 20px 0
+
+  input
+    background: transparent
+    line-height: 1.5em
+    border-bottom: 1px solid rgba(#fff, .4)
+    color: white
+    padding: 3px 0
+
+    &:focus
+      border-bottom-color: white
+
+  button
+    @apply text-white
+    margin: 10px
+    line-height: 1.5em
+    padding: 4px
+
+    &:hover,
+    &.active
+      border-bottom: 1px solid white
+      padding-bottom: 3px
 
 </style>
 
