45e6e5f44ef46246b33614c0b8bbecc353f2cf79
Andy home <huebner@system-provider.com>
2020-06-02 14:08:12 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,6 @@
         <h2>to System Provider!</h2>
         <h5>our office is temporarily closed.</h5>
         <h5>please contact us via mail & phone.</h5>
-        <h5>. </h5>
-        <h6>RIP Michael Brenner † </h6>
       </div>
     </template>
     <section class="md:flex justify-center">
