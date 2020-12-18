8aa3e8a4d54e77918d5fdb01c4adead84bee0887
Milena Gutdeutsch <milena@system-provider.com>
2020-12-18 09:15:45 +0100

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -97,7 +97,7 @@
           <br />
           <h2>Mitglied bei:</h2>
           <br />
-           <img src="http://livecom.at/wp-content/uploads/2020/11/Coming-Soon-white-on-black.001.png">
+           <img src="http://livecom.at/wp-content/uploads/2020/11/Coming-Soon-white-on-black.001.png" height="135px" width="375px">
           </p>
           <img src="https://www.oethg.at/wp-content/uploads/2019/02/oethg_logo.png">
           </p>
