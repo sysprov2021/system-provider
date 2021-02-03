48a8c04f6bf8288840c4ed435ff2401bde2a950a
Milena Gutdeutsch <milena@system-provider.com>
2021-02-03 13:41:00 +0100

    Update index.vue

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -98,7 +98,7 @@
           <br />
           <h2>Mitglied bei:</h2>
           <br />
-           <img src="file:///Users/milenagutdeutsch/Documents/livecom_logo1.png" height="108px" width="300px">
+           <img src="file:///Users/milenagutdeutsch/Documents/livecom_logo1.png">
           </p>
           <img src="https://www.oethg.at/wp-content/uploads/2019/02/oethg_logo.png" height="173,16px" width="302px">
           </p>
